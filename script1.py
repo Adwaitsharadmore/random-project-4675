791bc554bd45c5f88a9dee5ea50e7424290f9a88
Adwaitsharadmore <amore14@asu.edu>
2024-05-26 19:03:57 +0530

    Initial commit

---
+++ script1.py
@@ -0,0 +1,8 @@
+
+# Random Python Script
+
+def aayqjhjuxt():
+    print("This is a random function called aayqjhjuxt.")
+
+if __name__ == "__main__":
+    aayqjhjuxt()
---
+++ script2.py
@@ -0,0 +1,8 @@
+
+# Random Python Script
+
+def wbursqxwet():
+    print("This is a random function called wbursqxwet.")
+
+if __name__ == "__main__":
+    wbursqxwet()
